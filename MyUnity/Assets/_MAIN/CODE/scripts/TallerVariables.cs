08fa32746389f9319e18e08947213f791cbd395d
UnaFanny <sosorio576@soyudemedellin.edu.co>
2025-08-05 11:14:08 -0500

    tallerVariables

---
+++ MyUnity/Assets/_MAIN/CODE/scripts/TallerVariables.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using TMPro;
+
+public class tallervariables : MonoBehaviour
+{
+    [Header("Variables C#")]
+
+    [SerializeField]
+    private int vida = 1;
+    [SerializeField]
+    private int puntaje = 200;
+    [SerializeField]
+    private int nivel = 29;
+
+    public string nombre = "Stefany";
+    [SerializeField]
+    private float tiempo = 60.5f;
+    [SerializeField]
+    private bool isAlive = true;
+
+    [Header("Variables Unity")]
+
+    [SerializeField]
+    private TMP_Text nombreText;
+    [SerializeField]
+    private TMP_Text vidaText;
+    [SerializeField]
+    private TMP_Text tiempoText;
+    [SerializeField]
+    private TMP_Text puntostext;
+    [SerializeField]
+    private TMP_Text estaVivotext;
+
+
+    //Declarar las variables privadas y serializadas
+    
+    //Nombre
+    //Vida
+    //Tiempo
+    //Puntos
+    //EstaVivo
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nombreText.text = "Nombre: " + nombre;
+        vidaText.text = "Vida: " + vida;
+        tiempoText.text = "Tiempo: " + tiempo;
+        puntostext.text = "Puntos: " + puntaje;
+        estaVivotext.text = "Esta Vivo: " + isAlive;
+        //Le voy a dar un valor a nuestras variables de texto = cadena texto + variables
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
